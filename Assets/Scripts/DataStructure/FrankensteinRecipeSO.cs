bf00afeaf6e6667ae7d3eb709a0d1b1de84f4b9d
Leonardo Verrone <leo.kalcris@gmail.com>
2025-10-17 22:27:33 -0300

    remoção de parte desnecessaria do codigo

--- Assets/Scripts/DataStructure/FrankensteinRecipeSO.cs
+++ Assets/Scripts/DataStructure/FrankensteinRecipeSO.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class FrankensteinRecipeSO : ScriptableObject
 {
     public string recipeName;
-    public int requiredScore = 5;
+   // public int requiredScore = 5;
     
     [Header("Required Parts")]
     public FrankensteinPartSO leftArm;
@@ -15,19 +15,18 @@ public class FrankensteinRecipeSO : ScriptableObject
     public FrankensteinPartSO leftLeg;
     public FrankensteinPartSO rightLeg;
     
-    public int GetCurrentScore(FrankensteinMonster monster)
-    {
-        int score = 0;
-        if (monster.leftArm != null && monster.leftArm == leftArm) score += monster.leftArm.qualityScore;
-        if (monster.rightArm != null && monster.rightArm == rightArm) score += monster.rightArm.qualityScore;
-        if (monster.brain != null && monster.brain == brain) score += monster.brain.qualityScore;
-        if (monster.leftLeg != null && monster.leftLeg == leftLeg) score += monster.leftLeg.qualityScore;
-        if (monster.rightLeg != null && monster.rightLeg == rightLeg) score += monster.rightLeg.qualityScore;
-        return score;
-    }
+    //public int GetCurrentScore(FrankensteinMonster monster)
+    //{        int score = 0;
+    //    if (monster.leftArm != null && monster.leftArm == leftArm) score += monster.leftArm.qualityScore;
+    //    if (monster.rightArm != null && monster.rightArm == rightArm) score += monster.rightArm.qualityScore;
+     //   if (monster.brain != null && monster.brain == brain) score += monster.brain.qualityScore;
+     //   if (monster.leftLeg != null && monster.leftLeg == leftLeg) score += monster.leftLeg.qualityScore;
+    //    if (monster.rightLeg != null && monster.rightLeg == rightLeg) score += monster.rightLeg.qualityScore;
+    //    return score;
+   // }
     
-    public bool IsRecipeComplete(FrankensteinMonster monster)
-    {
-        return GetCurrentScore(monster) >= requiredScore;
-    }
+   // public bool IsRecipeComplete(FrankensteinMonster monster)
+    //{
+     //   return GetCurrentScore(monster) >= requiredScore;
+  //  }
 }
